6c7bac34b336d871c6dcd6eed0134a28fba03336
showmik <77013640+Miskat-UL@users.noreply.github.com>
2021-07-22 00:20:18 +0600

    datUPP

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const search = document.querySelector('.search')
 const btn = document.querySelector('.btn')
 const input = document.querySelector('.input')
-
+const date = document.getElementById('date')
 
 btn.addEventListener('click', () =>{
     search.classList.toggle('active')
     input.focus()
 })
+
+var d = new Date();
+var n = d.getFullYear();
+date.innerHTML= n
